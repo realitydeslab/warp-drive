180952bc61e90ceba5cd42315dd90c6ab1058782
030909Hh <3210601074@caa.edu.cn>
2023-12-12 16:05:42 +0800

    Fix

---
+++ Assets/WarpDrive/Scripts/ImageTrackingObjectManager.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class ImageTrackingObjectManager : MonoBehaviour
+{
+    [SerializeField]
+    OriginReseter _originReseter;
+
+    [SerializeField]
+    [Tooltip("Image manager on the AR Session Origin")]
+    ARTrackedImageManager m_ImageManager;
+
+    /// <summary>
+    /// Get the <c>ARTrackedImageManager</c>
+    /// </summary>
+    public ARTrackedImageManager ImageManager
+    {
+        get => m_ImageManager;
+        set => m_ImageManager = value;
+    }
+
+    [SerializeField]
+    [Tooltip("Reference Image Library")]
+    XRReferenceImageLibrary m_ImageLibrary;
+
+    /// <summary>
+    /// Get the <c>XRReferenceImageLibrary</c>
+    /// </summary>
+    public XRReferenceImageLibrary ImageLibrary
+    {
+        get => m_ImageLibrary;
+        set => m_ImageLibrary = value;
+    }
+
+    [SerializeField]
+    [Tooltip("Prefab for tracked 1 image")]
+    GameObject m_OnePrefab;
+
+    /// <summary>
+    /// Get the one prefab
+    /// </summary>
+    public GameObject onePrefab
+    {
+        get => m_OnePrefab;
+        set => m_OnePrefab = value;
+    }
+
+    GameObject m_SpawnedOnePrefab;
+    
+    static Guid s_FirstImageGUID;
+
+    void OnEnable()
+    {
+        s_FirstImageGUID = m_ImageLibrary[0].guid;
+        m_ImageManager.trackedImagesChanged += ImageManagerOnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_ImageManager.trackedImagesChanged -= ImageManagerOnTrackedImagesChanged;
+    }
+
+    void ImageManagerOnTrackedImagesChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        // added, spawn prefab
+        foreach(ARTrackedImage image in obj.added)
+        {
+            if (image.referenceImage.guid == s_FirstImageGUID)
+            {
+                m_SpawnedOnePrefab = Instantiate(m_OnePrefab, image.transform.position, image.transform.rotation);
+            }
+        }
+        
+        // updated, set prefab position and rotation
+        foreach(ARTrackedImage image in obj.updated)
+        {
+            // image is tracking or tracking with limited state, show visuals and update it's position and rotation
+            if (image.trackingState == TrackingState.Tracking)
+            {
+                if (image.referenceImage.guid == s_FirstImageGUID)
+                {
+                    Transform trackedImageTransform = image.transform;
+                    trackedImageTransform.rotation *= Quaternion.Euler(90f, 0f, 0f);
+
+                    m_SpawnedOnePrefab.transform.SetPositionAndRotation(trackedImageTransform.position, trackedImageTransform.rotation);
+
+                    _originReseter.OnImageTracked(trackedImageTransform);
+                    
+                }
+            }
+            // image is no longer tracking, disable visuals TrackingState.Limited TrackingState.None
+            else
+            {
+                if (image.referenceImage.guid == s_FirstImageGUID)
+                {
+                    
+                }
+            }
+        }
+        
+        // removed, destroy spawned instance
+        foreach(ARTrackedImage image in obj.removed)
+        {
+            if (image.referenceImage.guid == s_FirstImageGUID)
+            {
+                Destroy(m_SpawnedOnePrefab);
+            }
+        }
+    }
+
+}
